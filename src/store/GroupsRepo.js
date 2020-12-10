781e5cfcfbd98ce0d5eb702aee2dc72b96db36d8
KrasnovDenis <Elo.1972@yandex.ru>
2020-12-11 03:14:22 +0400

    removed: StudentController.js and GroupController

---
+++ src/store/GroupsRepo.js
@@ -0,0 +1,22 @@
+export function getGroupById (groupId) {
+
+}
+
+export function getAllGroups() {
+
+}
+
+export function addStudentToGroup(groupId, studentId) {
+
+}
+
+export function deleteStudentFromGroup(groupId, studentId) {
+
+}
+
+export function removeGroup(id) {
+
+}
+
+
+
